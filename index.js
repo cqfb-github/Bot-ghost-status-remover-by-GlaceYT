9e94ba7a7dc14b77762093198450189fdc7bd3e5
cqfb-github <ch4rlieb1@gmail.com>
2024-11-30 23:05:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["ashakaboomboom", "🎮 Playing VALORANT"];
+const statusMessages = ["m4jays is black", "@cb2554"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
